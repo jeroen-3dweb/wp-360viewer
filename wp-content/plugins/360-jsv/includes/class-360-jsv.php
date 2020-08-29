e12431f59020c5fcaf6169246e740a2558de6c8e
Jeroen Termaat <j.termaat@bookunited.com>
2020-08-29 11:24:12 +0200

    first commit

--- wp-content/plugins/360-jsv/includes/class-360-jsv.php
+++ wp-content/plugins/360-jsv/includes/class-360-jsv.php
@@ -3,16 +3,26 @@
 class JSV
 {
 
+    /**
+     * @var string
+     */
     private $version;
+
     /**
      * @var string
      */
     private $pluginName;
+
     /**
      * @var JSV_Loader
      */
     private $loader;
 
+    /**
+     * JSV constructor.
+     *
+     * @since 1.0.0
+     */
     public function __construct()
     {
         if (defined('JSV_VERSION')) {
@@ -28,34 +38,14 @@ class JSV
     }
 
     /**
-     * Load the required dependencies for this plugin.
+     * Load dependencies
      *
-     * Include the following files that make up the plugin:
-     *
-     * - Plugin_Name_Loader. Orchestrates the hooks of the plugin.
-     * - Plugin_Name_i18n. Defines internationalization functionality.
-     * - Plugin_Name_Admin. Defines all hooks for the admin area.
-     * - Plugin_Name_Public. Defines all hooks for the public side of the site.
-     *
-     * Create an instance of the loader which will be used to register the hooks
-     * with WordPress.
-     *
-     * @since    1.0.0
-     * @access   private
+     * @since 1.0.0
      */
     private function loadDependencies()
     {
-        /**
-         * The class responsible for orchestrating the actions and filters of the
-         * core plugin.
-         */
         require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-360-jsv-loader.php';
         require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-360-jsv-parser.php';
-
-        /**
-         * The class responsible for defining all actions that occur in the public-facing
-         * side of the site.
-         */
         require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-360-jsv-public.php';
 
         $this->loader = new JSV_Loader();
@@ -71,7 +61,7 @@ class JSV
     private function definePublicHooks()
     {
         $pluginPublic = new JSV_Public($this->pluginName, $this->version);
-        $parser = new JSV_Parser($this->pluginName, $this->version);
+        $parser       = new JSV_Parser($this->pluginName, $this->version);
 
         $this->loader->add_action('wp_enqueue_scripts', $pluginPublic, 'enqueue_styles');
         $this->loader->add_action('wp_enqueue_scripts', $pluginPublic, 'enqueue_scripts');
---
+++ wp-content/plugins/360-jsv/public/js/jsv.js
@@ -0,0 +1,61 @@
+// Find all the nodes with class .jsv
+
+//  For each node create a new viewer and add them in window.jsvholder
+// Read data from data container
+
+class jsvInstance {
+
+    constructor(node) {
+        this.data = this.getData(node);
+        this.mainHolderId = this.getMainHolderId(node);
+        this.mainImageId = this.getMainImageId(node);
+
+        this.run();
+    }
+
+    getMainImageId(){
+
+    }
+    getMainHolderId(node){
+      return node.id;
+    }
+    getData(node){
+        let data = {};
+        [].forEach.call(node.attributes, function(attr) {
+            if (/^data-/.test(attr.name)) {
+                var camelCaseName = attr.name.substr(5).replace(/-(.)/g, function ($0, $1) {
+                    return $1.toUpperCase();
+                });
+                data[camelCaseName] = attr.value;
+            }
+        });
+        return data;
+    }
+    run() {
+        const data = this.getData(this.node)
+
+        const jsv = new JavascriptViewer({
+            mainHolderId: this.getMainHolderId(),
+            mainImageId: this.getMainImageId(),
+            totalFrames: data.totalFrames,
+            defaultProgressBar: true,
+            speed: 90,
+            inertia: 12
+        });
+
+        jsv.start();
+    }
+
+}
+
+window.JSV = {
+    items: [],
+    errors: []
+};
+
+window.addEventListener('load', () => {
+    const nodes = document.getElementsByClassName("jsv-holder");
+    for (let index = 0; index < nodes.length; ++index) {
+        window.JSV.items.push(a[index]);
+    }
+});
