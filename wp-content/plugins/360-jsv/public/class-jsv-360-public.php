f8050a4a17b797913da21fe8442ae1001434c224
Jeroen Termaat <j.termaat@bookunited.com>
2020-09-17 16:15:11 +0200

    added missing files

---
+++ wp-content/plugins/360-jsv/public/class-jsv-360-public.php
@@ -0,0 +1,57 @@
+<?php
+
+class JSV_360_Public
+{
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $plugin_name The ID of this plugin.
+     */
+    private $plugin_name;
+
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $version The current version of this plugin.
+     */
+    private $version;
+
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @param string $plugin_name The name of the plugin.
+     * @param string $version The version of this plugin.
+     * @since    1.0.0
+     */
+    public function __construct($plugin_name, $version)
+    {
+        $this->plugin_name = $plugin_name;
+        $this->version     = $version;
+    }
+
+    /**
+     * Register the stylesheets for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles()
+    {
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/jsv.css', array(), $this->version, 'all');
+    }
+
+    /**
+     * Register the JavaScript for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts()
+    {
+        var_dump('inser script');
+        wp_enqueue_script('javascriptviewer', plugin_dir_url(__FILE__) . 'js/JavascriptViewer.min.js', array(), '0.0.37');
+        wp_enqueue_script('jsv360', plugin_dir_url(__FILE__) . 'js/jsv.js', array('javascriptviewer'), '1.0');
+    }
+}
