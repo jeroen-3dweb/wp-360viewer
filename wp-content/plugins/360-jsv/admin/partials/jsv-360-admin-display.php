2b9792bc8d5e10b4483330bffa37a48e33c907e6
Jeroen Termaat <j.termaat@bookunited.com>
2020-11-16 12:35:38 +0100

    missing file

---
+++ wp-content/plugins/360-jsv/admin/partials/jsv-360-admin-display.php
@@ -0,0 +1,23 @@
+<h1>360 Javascript Viewer</h1>
+<h2>Steps to create the 360 presentation</h2>
+
+<ol>
+    <li>Create your presentation at <a target="_blank" href="https://www.360-javascriptviewer.com/wordpress">360-javascriptviewer.com/wordpress</a>
+    </li>
+    <li>If all setting are right generate your shortcode there, something like this:<br>
+        <code>[360-jsv total-frames=72
+            main-image-url=https://cdn1.360-javascriptviewer.com/images/blue-shoe-small/20180906-001-blauw.jpg
+            image-url-format=20180906-0xx-blauw.jpg speed=90 inertia=12 zoom=false reverse=true auto-rotate=1 ] </code>
+    </li>
+    <li>Insert the code somewhere in your page, post or widget</li>
+    <li>Position your presentation with the options below <br>
+        <ul>
+           <li>max-width sets the max-width of the presentation in px</li>
+            <li>float use left or right to align the presentation</li>
+            <li>margin-left set the left margin</li>
+            <li>margin-right set the right margin</li>
+            <li>margin-top set the top margin</li>
+            <li>margin-bottom set the bottom margin</li>
+        </ul>
+    </li>
+</ol>
