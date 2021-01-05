53819540213db6cf7c686380681d62aa65c489da
Jeroen Termaat <j.termaat@bookunited.com>
2021-01-05 17:10:20 +0100

    wip

---
+++ wp-content/plugins/360-jsv/includes/woo/class-jsv-360-woo-metabox.php
@@ -0,0 +1,94 @@
+<?php
+
+class JSV_360_WOO_METABOX
+{
+
+    const FIELD_BBCODE = 'jsv_360_woo_bbcode_';
+
+    const FIELD_NONCE = 'jsv_360_woo_nonce_';
+
+    /**
+     * @var string $pluginName
+     */
+    private $pluginName;
+
+    /**
+     * @var string $version
+     */
+    private $version;
+
+    /**
+     * JSV_Parser constructor.
+     * @param $pluginName
+     * @param $version
+     */
+    public function __construct($pluginName, $version)
+    {
+        $this->pluginName = $pluginName;
+        $this->version    = $version;
+    }
+
+
+    public function addBoxes($post_type)
+    {
+        if (in_array($post_type, ['product'])) {
+            add_meta_box(
+                'sr-product-360-view',
+                '360 Javascript Viewer Code',
+                array(
+                    self::class,
+                    'jsv_360_woo_product_360_view_callback'
+                ),
+                $post_type,
+                'normal',
+                'core'
+            );
+        }
+    }
+
+    public function saveBoxes($post_id)
+    {
+        if (!isset($_POST[self::FIELD_NONCE]) || (empty($_POST[self::FIELD_NONCE]) || !wp_verify_nonce(
+                    $_POST[self::FIELD_NONCE],
+                    basename(__FILE__)
+                ))) {
+            return;
+        }
+
+        if (!current_user_can('edit_post', $post_id)) {
+            return;
+        }
+
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+
+        if (isset($_POST[self::FIELD_BBCODE])) {
+            update_post_meta($post_id, self::FIELD_BBCODE, $_POST[self::FIELD_BBCODE]);
+        } else {
+            delete_post_meta($post_id, self::FIELD_BBCODE);
+        }
+    }
+
+    static function jsv_360_woo_product_360_view_callback($post)
+    {
+        wp_nonce_field(basename(__FILE__), self::FIELD_NONCE);
+        $bbCode = sanitize_meta(self::FIELD_BBCODE, get_post_meta($post->ID, self::FIELD_BBCODE, true), 'post');
+        ?>
+        <table class="form-table">
+            <tr>
+                <td>
+                    <div>
+                        <strong><?= __('Shortcode:', 'jsv_360_view'); ?>
+                            <a target="_blank" href="https://www.360-javascriptviewer.com/wordpress" title="create code online">
+                                tool for creating code
+                            </a> </strong>
+                        <input class="jsv-360-woo-bbcode" type="text" name="<?= self::FIELD_BBCODE ?>"
+                               value="<?= $bbCode ?>"/>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <?php
+    }
+}
---
+++ wp-content/plugins/360-jsv/public/class-jsv-360-public-woo.php
@@ -0,0 +1,82 @@
+<?php
+
+class JSV_360_Public_Woo
+{
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $plugin_name The ID of this plugin.
+     */
+    private $plugin_name;
+
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $version The current version of this plugin.
+     */
+    private $version;
+
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @param string $plugin_name The name of the plugin.
+     * @param string $version The version of this plugin.
+     * @since    1.0.0
+     */
+    public function __construct($plugin_name, $version)
+    {
+        $this->plugin_name = $plugin_name;
+        $this->version     = $version;
+    }
+
+    private function generateId() {
+        $permitted_chars = implode('', range('a', 'z'));
+        return 'jsv-' .  '-' . substr(str_shuffle($permitted_chars), 0, 10);
+    }
+
+    public function add_360_icon($d, $e)
+    {
+        if ($bbCode = $this->getBBCode()) {
+            global $product;
+            $attachment_ids = $product->get_gallery_image_ids();
+
+            if ($e == end($attachment_ids)) {
+                $html = (new JSV_360_Parser($this->plugin_name, $this->version))->parse($bbCode);
+                $htmlLarge = (new JSV_360_Parser($this->plugin_name, $this->version))->parse($bbCode);
+                $randomId = $this->generateId();
+
+                $mainImage  = 'https://placehold.it/600';
+                $thumbImage = 'https://placehold.it/100';
+                $text       =
+                    <<<EOD
+                    <div data-thumb="$thumbImage"  class="woocommerce-product-gallery__image">
+                        <script lang="text/javascript">loadjsv(`$html`, `$randomId`,'$htmlLarge')</script>
+                          <a href="$mainImage" id="$randomId">
+                            <img width="416" height="312"
+                            src="" 
+                            class="wp-post-image" alt="" loading="lazy" title="blauw" data-caption="" 
+                            data-large_image="$mainImage" 
+                            data-large_image_width="640" 
+                            data-large_image_height="480" />
+                        </a>
+                    </div>                    
+                    EOD;
+
+
+                return $d .$text;
+            }
+        }
+        return $d;
+    }
+
+    private function getBBCode()
+    {
+        /** @var WC_Product $product */
+        global $product;
+        return $product->get_meta(JSV_360_WOO_METABOX::FIELD_BBCODE) ?: null;
+    }
+}
